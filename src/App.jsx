d49fbd4047b545bd5cd5e3f3e00ded73d5cdbe7b
espoir dev <saloudiallo151@gmail.com>
2026-04-19 06:10:17 +0200

    feat: configuration point d'entrée React (main.jsx + App.jsx)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dossier from './components/Dossier.jsx'
+
+function App() {
+  return (
+    <div style={{ color: 'white', padding: '2rem' }}>
+      <Dossier />
+    </div>
+  )
+}
+
+export default App
